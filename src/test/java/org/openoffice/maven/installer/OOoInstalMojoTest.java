1122155d74f287ee9f4f7ec3a2cace3554fa5d52
Oli B. <boehm@javatux.de>
2010-08-28 17:11:42 +0200

    JUnit test for OOoInstalMojo added

---
+++ src/test/java/org/openoffice/maven/installer/OOoInstalMojoTest.java
@@ -0,0 +1,62 @@
+/*************************************************************************
+ * OOoInstalMojoTest.java
+ *
+ * The Contents of this file are made available subject to the terms of
+ * either of the GNU Lesser General Public License Version 2.1
+ * 
+ * GNU Lesser General Public License Version 2.1
+ * =============================================
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ *
+ * Contributor(s): oliver.boehm@agentes.de
+ ************************************************************************/
+package org.openoffice.maven.installer;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.openoffice.maven.AbstractMojoTest;
+
+/**
+ * JUnit test for OOoInstalMojo.
+ * 
+ * @author oliver (oliver.boehm@agentes.de)
+ * @since 1.1.1 (28.08.2010)
+ */
+public class OOoInstalMojoTest extends AbstractMojoTest {
+    
+    /**
+     * Set up the mojo.
+     * 
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        mojo = (OOoInstalMojo) lookupMojo("install", TEST_POM);
+        assertNotNull(mojo);
+        this.setUpMojo();
+        this.setUpProject4Mojo();
+    }
+
+    /**
+     * Test method for {@link OOoInstalMojo#execute()}.
+     *
+     * @throws MojoExecutionException the mojo execution exception
+     * @throws MojoFailureException the mojo failure exception
+     * @throws IllegalAccessException the illegal access exception
+     */
+    public void testExecute() throws MojoExecutionException, MojoFailureException, IllegalAccessException {
+        mojo.execute();
+    }
+
+}
